0c137452071ed508cd02302db0cbce96c00422ad
shuvro baset <md.abdul.baset75@gmail.com>
2021-11-01 23:01:46 +0600

    sdf

--- src/Components/Teacher/Teacher.js
+++ src/Components/Teacher/Teacher.js
@@ -11,11 +11,13 @@ const Teacher = (props) => {
             <div className="d-flex justify-content-center my-3">
                 <img src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" className="img-fluid teacher-img" alt="" />
             </div>
-            <p>  <b>Name:</b> {name}</p>
-            <p>  <b>Designation:</b> {designation} </p>
-            <p>  <b>Subject:</b> {subject}</p>
-            <p>  <b>Qualification:</b> {Qualification}</p>
-            <p>  <b>Lecture Number:</b> {number_of_lecture_per_month} </p>
+            <div>
+                <p>  <b>Name:</b> {name}</p>
+                <p>  <b>Designation:</b> {designation} </p>
+                <p>  <b>Subject:</b> {subject}</p>
+                <p>  <b>Qualification:</b> {Qualification}</p>
+                <p>  <b>Lecture Number:</b> {number_of_lecture_per_month} </p>
+            </div>
 
         </Card>
     );
